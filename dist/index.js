564394f5a845f06e7a8f3edd606a352f07300d72
Fozi Chris <kamufozi679@gmail.com>
2025-06-01 22:11:24 +0200

    typescript 1hr youtube tutorial

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let sales = 123_456_654;
+let course = 'TypeScript';
+let is_publiched = true;
+console.log(sales);
+let numbers = [1, 2];
+let tuple = [1, 'fozi'];
+tuple.push('hello');
+function calculate(income) {
+    console.log('hey');
+    return 0;
+}
+calculate(500);
+//# sourceMappingURL=index.js.map
