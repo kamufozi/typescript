dfb5524c8943b4f60afe68e5cf24749d63bc8d3b
kamufozi <kamufozi679@gmail.com>
2025-06-05 09:45:40 +0200

    Seen the way tuple work

--- okee.js
+++ okee.js
@@ -1,2 +1,3 @@
 let numbers=[1,2]
-numbers.forEach(i=>i.)
+// numbers.forEach(i=>i.)
+let mama =[23,4,5,4]
--- src/output.ts
+++ src/output.ts
@@ -1,2 +1,12 @@
 let file=JSON.parse('55');
-console.log(file,file.type)
+console.log(file,file.type);
+// let u = true;
+// u = "string"; // Error: Type 'string' is not assignable to type 'boolean'.
+// Math.round(u); // Error: Argument of type 'boolean' is not assignable to parameter of type 'number'.
+
+let myArr: readonly string[] =['jacob']
+console.log(myArr)
+
+let tupple:readonly [string,number,boolean]=['keke',99,true];
+
+
